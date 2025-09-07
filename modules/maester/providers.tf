18ebd70a7ee41cca15188ef27fb105967010a118
Charles flannery <charlesflannery73@gmail.com>
2025-09-08 09:38:28 +1000

    move providers to the module

--- modules/maester/providers.tf
+++ modules/maester/providers.tf
@@ -4,5 +4,15 @@ terraform {
       source  = "azure/azapi"
       version = ">=1.12.0"
     }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.80.0, < 4.0.0"
+    }
   }
 }
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+}
+
